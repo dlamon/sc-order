8ed8541448a1e16fd4eadff298a6e73c3a2fe626
廖伟 <24591106@qq.com>
2019-06-20 00:54:08 +0800

    提交代码

--- server/src/main/java/cn/net/liaowei/sc/order/ScOrderApplication.java
+++ server/src/main/java/cn/net/liaowei/sc/order/ScOrderApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
+@EnableFeignClients(basePackages = "cn.net.liaowei.sc.product.client")
 @EnableSwagger2Doc
 public class ScOrderApplication {
 
