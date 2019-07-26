5d1228ecbee3435a868939e23783dd5734dd0453
廖伟 <24591106@qq.com>
2019-07-26 17:41:19 +0800

    提交代码

--- server/src/main/java/cn/net/liaowei/sc/order/controller/EnvController.java
+++ server/src/main/java/cn/net/liaowei/sc/order/controller/EnvController.java
@@ -7,6 +7,7 @@ import io.swagger.annotations.ApiOperation;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author LiaoWei
  */
 @RestController
+@RefreshScope
 @Slf4j
 @Getter
 @Api(tags = "/env", description = "环境变量服务")
